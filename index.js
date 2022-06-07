b45b0b18d3f0ea7afd4e3762479ed48a99c0ccf4
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-07 15:31:05 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+//defines saturdayFun function declaration
+//default activity 'roller-skate'
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+  //defines mondayWork function expression
+  //default activity 'go to the office'
+  const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  
+  //defines wrapAdjective function
+  //when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", 
+  const wrapAdjective = function(string="*") {
+    return function(adjective="special") {
+      return `You are ${string}${adjective}${string}!`
+    }
+  }
+
